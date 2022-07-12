1327c28447c669d0c3df28ef294b69514dacbcaa
AoutikArya <90469048+AoutikArya@users.noreply.github.com>
2022-07-12 11:20:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 import nltk
 from nltk.stem import PorterStemmer, WordNetLemmatizer
 from nltk.corpus import stopwords
-from tensorflow import keras
+#from tensorflow import keras
 from keras.preprocessing.text import one_hot
 from keras.preprocessing.sequence import pad_sequences
 from PIL import Image
